e7db853da98b20db9c1d6b85a2b5c7bd83d70e91
zayson <jymaeng95@gmail.com>
2022-07-05 17:51:25 +0900

    fix: 닉네임 유효성 검증 구현

--- src/main/java/com/dev/nbbang/auth/authentication/util/NicknameValidation.java
+++ src/main/java/com/dev/nbbang/auth/authentication/util/NicknameValidation.java
@@ -3,11 +3,11 @@ package com.dev.nbbang.auth.authentication.util;
 import com.dev.nbbang.auth.authentication.exception.IllegalNicknameException;
 import com.dev.nbbang.auth.global.exception.NbbangException;
 
+import java.util.regex.Pattern;
+
 public class NicknameValidation {
     public static Boolean valid(String nickname) {
         // 특수문자 및 공백 제외
-        if(!nickname.matches("[^\\wㄱ-힣]|[\\_]") || nickname.length() < 3 || nickname.length() > 10) return false;
-
-        return true;
+        return Pattern.compile("[^a-zA-Z0-9ㄱ-힣]").matcher(nickname).find() || nickname.length() < 3 || nickname.length() > 10;
     }
 }
